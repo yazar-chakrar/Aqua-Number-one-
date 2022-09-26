af804252a44267a00d4eb4918db7478731f8c0be
yazarchakrar <hm_chakrar@esi.dz>
2022-09-26 16:13:26 +0100

    api v2 - generics CV

--- foods/api.py
+++ foods/api.py
@@ -1,4 +1,5 @@
 from unicodedata import category
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import *
@@ -8,20 +9,27 @@ from .models import Food
 from rest_framework import generics
 
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def food_list_api(request):
-    all_foods = Food.objects.all()
-    data = FoodSerializer(all_foods, many=True).data
-    context = {'food-list':data}
-    return Response(context)
+    if request.method == 'GET':
+        all_foods = Food.objects.all()
+        serializer = FoodSerializer(all_foods, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = FoodSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
 def food_category_api(request, cat):
-    food_detail = Food.objects.filter(category=cat)
-    data = FoodSerializer(food_detail, many=True).data
-    context = {'food-list':data}
-    return Response(context)
+    if request.method == 'GET':
+        food_detail = Food.objects.filter(category=cat)
+        serializer = FoodSerializer(food_detail, many=True)
+        return Response(serializer.data)
 
 
 #############################################
